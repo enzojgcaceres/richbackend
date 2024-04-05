08c97b43a33c68a1377aa525373ba4b7491f8dd3
enzojgcaceres <enzojgcaceres@gmail.com>
2024-04-05 18:14:21 -0300

    Cambios para el Dploy DB

--- DB_connection.js
+++ DB_connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const FavoriteModel = require("./models/Favorite")
 const UserModel = require("./models/User");
 
@@ -9,9 +9,17 @@ const UserModel = require("./models/User");
 // Recuerda pasarle la información de tu archivo '.env'.
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/rickandmorty`,
-   { logging: false, native: false }
+const sequelize = new Sequelize( DB_DEPLOY, 
+   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/rickandmorty`,
+   { 
+      logging: false,
+      native: false,
+      dialectOptions: {
+         ssl: {
+            require: true,
+         }
+      } 
+   }
 );
 
 // EJERCICIO 05
